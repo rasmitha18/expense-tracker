991d758b6fe34d68882c258bd777cded9955a5e0
unknown <rasmithadev@gmail.com>
2026-07-06 19:03:28 +0530

    to-do list

---
+++ test.js
@@ -0,0 +1,12 @@
+let array = [1,[2,[4],5],6];
+
+function flatten(array) {
+    let temp = array;
+    for(let i=0;i<temp.length;i++) {
+        console.log(temp[i]);
+        if(i === typeof)
+    }
+
+}
+
+flatten(array);
